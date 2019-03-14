fa8ed0f963d31a680f6f74b9c58168c1265a3e98
wu <hsnuhayato@gmail.com>
2019-03-14 21:39:22 +0900

    get rid of jython stub, use python stub instead.also use ninja to build source

--- rtc/CameraViewer/make.sh
+++ rtc/CameraViewer/make.sh
@@ -15,8 +15,8 @@ export PATH=/usr/bin:$PATH
 export PKG_CONFIG_PATH=/usr/lib/pkgconfig:$PKG_CONFIG_PATH
 export LD_LIBRARY_PATH=/usr/lib:$LD_LIBRARY_PATH
 
-#CMAKE_OPT="-DCMAKE_INSTALL_PREFIX:STRING=/usr/lib/choreonoid-1.5  -DCOMP_NAME=${COMP_NAME}"
-CMAKE_OPT="-DCMAKE_INSTALL_PREFIX:STRING=/home/player/tsml  -DCOMP_NAME=${COMP_NAME}"
+#CMAKE_OPT="-DCMAKE_INSTALL_PREFIX:STRING=/usr/lib/choreonoid-1.5  $@  -DCOMP_NAME=${COMP_NAME}"
+CMAKE_OPT="-DCMAKE_INSTALL_PREFIX:STRING=/home/player/tsml -DCOMP_NAME=${COMP_NAME} -DNO_COMP=1"
 MAKE_OPT="VERBOSE=1 $@"
 
 # for logging
@@ -24,10 +24,10 @@ DATE=`/bin/date '+%Y%m%d%H%M'`
 mkdir -p log
 
 # cmake
-cmake . ${CMAKE_OPT} 2>&1 | tee log/build_${DATE}.log
-
+#cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ${CMAKE_OPT} 2>&1 | tee log/build_${DATE}.log
+cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -G Ninja ${CMAKE_OPT} 2>&1 | tee log/build_${DATE}.log
 # make
-make ${MAKE_OPT} 2>&1 | tee -a log/build_${DATE}.log
+ninja 2>&1 | tee -a log/build_${DATE}.log
 
 cd log
 ln -sfv build_${DATE}.log build.log
